59905298d9bc7345df14b471a3a091c79ef3be22
Muhammad Talha <107686341+TalhaQadir@users.noreply.github.com>
2022-08-20 21:21:40 -0700

    completed github account api
    completed api and assignment

---
+++ api-github-json/app.js
@@ -0,0 +1,61 @@
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("login").innerHTML = data.login)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("id").innerHTML = data.id)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("node_id").innerHTML = data.node_id)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("avatar_url").innerHTML = data.avatar_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("gravatar_url").innerHTML = data.gravatar_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("url").innerHTML = data.url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("html_url").innerHTML = data.html_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("following_url").innerHTML = data.following_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("gists_url").innerHTML = data.gists_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("starred_url").innerHTML = data.starred_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("subcriptions_url").innerHTML = data.subcriptions_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("organizations_url").innerHTML = data.organizations_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("repos_url").innerHTML = data.repos_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("events_url").innerHTML = data.events_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("recieved_events_url").innerHTML = data.recieved_events_url)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("type").innerHTML = data.type)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("site_admin").innerHTML = data.site_admin)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("name").innerHTML = data.name)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("company").innerHTML = data.company)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("blog").innerHTML = data.blog)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("location").innerHTML = data.location)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("email").innerHTML = data.email)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("hireable").innerHTML = data.hireable)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("bio").innerHTML = data.bio)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("twitter_username").innerHTML = data.twitter_username)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("public_repos").innerHTML = data.public_repos)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("public_gists").innerHTML = data.public_gists)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("followers").innerHTML = data.followers)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("following").innerHTML = data.following)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("created_at").innerHTML = data.created_at)
+
+fetch("https://api.github.com/users/TalhaQadir").then(res=> res.json()).then(data=> document.getElementById("updated_at").innerHTML = data.updated_at)
---
+++ json-first-lesson/app.js
@@ -0,0 +1,96 @@
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.name))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.lastname))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.zip))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.phone))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.city))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.province))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.country))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.ethinicity))
+
+// Now calling  json in html in above we called it in console
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("name").innerHTML = data.name)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("lastname").innerHTML = data.lastname)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("zip").innerHTML = data.zip)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("phone").innerHTML = data.phone)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("city").innerHTML = data.city)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("province").innerHTML = data.province)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("country").innerHTML = data.country)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("ethinicity").innerHTML = data.ethinicity)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.name))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.lastname))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.zip))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.phone))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.city))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.province))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.country))
+
+// fetch("data.json").then(res=> res.json()).then(data=> console.log(data.ethinicity))
+
+// Now calling  json in html in above we called it in console
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("name").innerHTML = data.name)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("lastname").innerHTML = data.lastname)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("zip").innerHTML = data.zip)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("phone").innerHTML = data.phone)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("city").innerHTML = data.city)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("province").innerHTML = data.province)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("country").innerHTML = data.country)
+
+fetch("data.json").then(res=> res.json()).then(data=> document.getElementById("ethinicity").innerHTML = data.ethinicity)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
